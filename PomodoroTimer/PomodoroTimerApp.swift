fc12bb4103a7acd2f0da1cdddf0bd90f5f6b0acb
Ade131 <aidankelly202@gmail.com>
2022-10-16 21:24:05 +0100

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Aidan Kelly on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
